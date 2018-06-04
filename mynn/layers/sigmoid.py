28899d82ac8852cd5d70718e6ea2f2c2df62a5fa
xuwei95 <1013104194@qq.com>
2018-06-04 18:43:29 +0800

    sigmoid

--- mynn/layers/sigmoid.py
+++ mynn/layers/sigmoid.py
@@ -6,13 +6,12 @@ class Sigmoid(object):
         self.x = np.zeros(shape)
         self.output_shape = shape
         self.layertype = 'sigmoid'
-    def sigmoid(self,x):
-        return 1 / (1 + np.exp(-x))
     def forward(self, x):
         self.x = x
-        return self.sigmoid(x)
+        self.output=1.0/(1.0+np.exp(-self.x))
+        return self.output
 
     def gradient(self, eta):
-        self.eta = eta
-        self.eta = self.eta * (1 - self.eta)
+        self.eta=eta
+        self.eta = self.eta*self.output*(1-self.output)
         return self.eta
